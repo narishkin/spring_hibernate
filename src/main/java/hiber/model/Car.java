a0fd72099887d9920f807b2194dfbeebf823f8cf
Konstantin Narishkin <narishun@gmail.com>
2021-09-05 13:41:12 +0300

    next

--- src/main/java/hiber/model/Car.java
+++ src/main/java/hiber/model/Car.java
@@ -6,22 +6,11 @@ import javax.persistence.*;
 @Table(name = "cars")
 public class Car {
 
-//    @OneToOne(fetch = FetchType.EAGER,cascade = CascadeType.ALL,mappedBy = "car")
-//    User user;
-
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "car_id")
     private long id;
-//
-//    public User getUser() {
-//        return user;
-//    }
-//
-//    public void setUser(User user) {
-//        this.user = user;
-//    }
+
 
     @Column(name = "car_model")
     private String model;
@@ -29,7 +18,8 @@ public class Car {
     @Column(name = "car_series")
     private int series;
 
-    public Car(){}
+    public Car() {
+    }
 
     public Car(String model, int series) {
 
